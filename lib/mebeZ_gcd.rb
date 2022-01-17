f9b13bd880e39b87c75cb1606a540c86bffe6718
Zachary Hine <zacharyhine@gmail.com>
2022-01-17 05:52:29 +0000

    Added GCD class

--- lib/mebeZ_gcd.rb
+++ lib/mebeZ_gcd.rb
@@ -2,7 +2,40 @@
 
 require_relative "mebeZ_gcd/version"
 
-module MebeZGcd
-  class Error < StandardError; end
-  # Your code goes here...
+class GCD
+  
+  attr_accessor :a
+  attr_accessor :b
+  
+  def initialize(a, b) 
+    @a = a
+    @b = b
+    if (b > a) 
+      swap()
+    end
+  end
+  
+  def swap
+    temp = @a
+    @a = @b
+    @b = temp
+  end
+  
+  def recurse
+    r = @a % @b 
+    @a = @b
+    @b = r
+  end
+  
+  def get_gcd # Apply Euclid's algorithm to get GCD
+    #base case R0
+    if (@a % @b == 0) 
+      return @b
+    end
+    # else recurse
+    recurse()
+    return get_gcd
+  end
 end
+
+gcd = GCD.new(3, 8)
--- test/test_mebeZ_gcd.rb
+++ test/test_mebeZ_gcd.rb
@@ -3,11 +3,18 @@
 require "test_helper"
 
 class TestMebeZGcd < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::MebeZGcd::VERSION
+  def test_init
+    t1 = GCD.new(8, 3)
+    assert t1.a > t1.b
   end
-
-  def test_it_does_something_useful
-    assert false
+  
+  def test_swap 
+    t2 = GCD.new(3, 8)
+    assert t2.a > t2.b
+  end
+  
+  def test_gcd 
+    t3 = GCD.new(27, 29) 
+    assert t3.get_gcd == 1
   end
 end
